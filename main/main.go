75c7032a6e6bb7623da062a1890f0e6e3c9d0d0d
jp <josephpanossian@gmail.com>
2022-08-30 10:52:50 -0700

    file handlers

--- main/main.go
+++ main/main.go
@@ -1,19 +1,22 @@
 package main
 
 import (
+	"flag"
 	"fmt"
 	"net/http"
-	"flag"
+	"os"
+
 	"github.com/josephpanossian/urlshort"
 )
 
 func main() {
-	var dataFormat = flag.String("dataF", "yaml", "The type of data input with format {url:key, value:path}, i.e. json, yaml}")
-
+	//input filename flag
+	var fName = flag.String("fName", "paths.yaml", "Specify file to parse with urls. If using json must also specify the -fType flag")
+	//could alternatively check extension type and use flag as fallback
+	var fType = flag.String("fType", "yaml", "Specify the type of file input. Supports yaml and json")
 	flag.Parse()
 
 	mux := defaultMux()
-
 	// Build the MapHandler using the mux as the fallback
 	pathsToUrls := map[string]string{
 		"/urlshort-godoc": "https://godoc.org/github.com/gophercises/urlshort",
@@ -21,43 +24,36 @@ func main() {
 	}
 	handler := urlshort.MapHandler(pathsToUrls, mux)
 
-	// Build the YAMLHandler using the mapHandler as the
-	// fallback
-	if (*dataFormat == "yaml") {
-	yaml := `
-- path: /urlshort
-  url: https://github.com/gophercises/urlshort
-- path: /urlshort-final
-  url: https://github.com/gophercises/urlshort/tree/solution
-`
-		var err error
-		handler, err = urlshort.YAMLHandler([]byte(yaml), handler)
+	//read file into byte slice
+	data, err := os.ReadFile(*fName)
+	if err != nil {
+		panic(err)
+	}
+
+	if *fType == "yaml" { //YAML handler with default mux as fallback
+		fmt.Printf("serving with yaml file: %s\n", *fName)
+		handler, err = urlshort.YAMLHandler(data, handler)
 		if err != nil {
 			panic(err)
 		}
-	// Build JSONHandler with handler as fallback
-	} else if (*dataFormat == "json") {
-		json := `[
-		{"path": "/josephpanossian", "url": "https://github.com/josephpanossian"},
-		{"path": "/gh", "url": "https://github.com"}
-		]
-		`
-		var err error
-		handler, err  = urlshort.JSONHandler([]byte(json), handler)
+	} else if *fType == "json" { //JSON handler with default mux as fallback
+		fmt.Printf("serving with json file: %s\n", *fName)
+		handler, err = urlshort.JSONHandler(data, handler)
 		if err != nil {
 			panic(err)
 		}
 	}
+	//start and serve
 	fmt.Println("Starting the server on :8080")
 	http.ListenAndServe(":8080", handler)
 }
-
+//default connection
 func defaultMux() *http.ServeMux {
 	mux := http.NewServeMux()
 	mux.HandleFunc("/", hello)
 	return mux
 }
-
+//default connection func handler
 func hello(w http.ResponseWriter, r *http.Request) {
 	fmt.Fprintln(w, "Hello, world!")
 }
